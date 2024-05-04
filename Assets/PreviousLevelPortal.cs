7574b1c8f45a2d01293a688cd368bb0f88d08a38
mak4la <mak4la@gmail.com>
2024-05-04 01:12:27 -0500

    Final Bug Fixes..

--- Assets/PreviousLevelPortal.cs
+++ Assets/PreviousLevelPortal.cs
@@ -8,7 +8,7 @@ public class PreviousLevelPortal : MonoBehaviour
 {
     private bool playerInRange = false;
     private float timer = 0f;
-    private float teleportDelay = 3f;
+    private float teleportDelay = 5f;
 
     private void Update()
     {
