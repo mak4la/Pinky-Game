293935303946146ef4b47cd7a65e11f7a6caa243
mak4la <mak4la@gmail.com>
2024-05-05 00:49:52 -0500

    Audio to Boss Monster
    audio bug fixes

--- Assets/BossMonster.cs
+++ Assets/BossMonster.cs
@@ -4,7 +4,21 @@ using UnityEngine.UI;
 
 public class BossMonster : MonoBehaviour
 {
+    public AudioClip collisionSound; // Reference to the collision sound
+    private AudioSource audioSource;
 
+    private void Start()
+    {
+        // Get the AudioSource component attached to this object
+        audioSource = GetComponent<AudioSource>();
+        // Add an AudioSource component if it doesn't exist
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        // Assign the collision sound to the AudioSource
+        audioSource.clip = collisionSound;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,6 +28,11 @@ public class BossMonster : MonoBehaviour
             //Kill();
             other.GetComponent<PlayerControl>().TakeDamage(1); // Damage the player by 1
 
+        }
+        if (other.CompareTag("Projectile"))
+        {
+            // Play the collision sound
+            audioSource.Play();
         }
     }
 
